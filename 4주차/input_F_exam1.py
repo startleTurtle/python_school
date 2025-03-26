73c8cb54dac6fe346e03c2c10a60b8ea0338d5b7
startleTurtle <startleTurt@gmail.com>
2025-03-26 15:54:34 +0900

    Add files via upload

---
+++ 4주차/input_F_exam1.py
@@ -0,0 +1,22 @@
+"""
+num1 = input("첫 번째 수 입력... ")
+num2 = input("두 번째 수 입력... ")
+
+num3 = int(num1) # 자료형 변경
+num4 = int(num2) # 자료형 변경
+
+print(num1,"+", num2,"=", num3 + num4)
+print(num1, "-", num2,"=", num3 - num4)
+print(num1, "*", num2,"=", num3 * num4)
+print(num1, "/", num2,"=", num3 / num4)
+"""
+
+num1 = int(input("첫 번째 수 입력... "))
+num2 = int(input("두 번째 수 입력... "))
+
+print(num1,"+", num2,"=", num1 + num2)
+print(num1, "-", num2,"=", num1 - num2)
+print(num1, "*", num2,"=", num1 * num2)
+print(num1, "/", num2,"=", num1 / num2)
+
+
