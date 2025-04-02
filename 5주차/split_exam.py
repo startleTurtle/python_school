fc6e89d3184bd18506e9a42b8f4804bd23d1d869
startleTurtle <startleTurt@gmail.com>
2025-04-02 16:39:09 +0900

    Add files via upload

---
+++ 5주차/split_exam.py
@@ -0,0 +1,12 @@
+s = "10,20,30,40"
+t = "10:20:30:40"
+u = "10 20 30 40"
+s_split=s.split(",")
+print(s_split)
+print(type(s_split))
+t_split = t.split(":")
+print(t_split)
+print(type(t_split))
+u_split = u.split(" ")
+print(u_split)
+print(type(u_split))
