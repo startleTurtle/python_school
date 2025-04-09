698377a19e1fada85a1304640a71d6da16ecb59e
startleTurtle <startleTurt@gmail.com>
2025-04-09 16:22:44 +0900

    Add files via upload

---
+++ 6주차/if_and_exam.py
@@ -0,0 +1,16 @@
+num = int(input("정수입력..."))
+
+if num >= 90 and num <= 100:
+    print("A")
+
+if num >= 80 and num < 90:
+    print("B")
+
+if num >= 70 and num <= 79:
+    print("C")
+
+if num >= 60 and num <= 69:
+    print("D")
+
+if num >= 0 and num <= 59:
+    print("F")
---
+++ 6주차/if_exam.py
@@ -0,0 +1,12 @@
+num = input("정수 입력...")
+num = int(num)
+#num = int(input("정수 입력..."))
+
+if num > 0:
+    print("양수입니다")
+
+if num < 0:
+    print("음수입니다")
+
+if num == 0:
+    print("0입니다")
